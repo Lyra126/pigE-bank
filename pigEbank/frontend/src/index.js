80483e2efe6e807395ec73be05365a6a6dfa2f68
rachel-pu <rpu@ufl.edu>
2024-02-02 18:16:45 -0500

    found and added the css whew

--- pigEbank/frontend/src/index.js
+++ pigEbank/frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App'; // Import the main App component
 
 ReactDOM.render(
   <React.StrictMode>
-    <App /> 
+    <App/> 
   </React.StrictMode>,
   document.getElementById('root')
 );
