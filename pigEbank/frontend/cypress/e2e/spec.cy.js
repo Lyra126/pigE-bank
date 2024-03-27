ab70456252947514b026ada22bd5fd267b37f15f
MichaelJK2143 <michaeljohnknauf@gmail.com>
2024-03-27 04:23:46 -0400

    improved login test to check all account logins

--- pigEbank/frontend/cypress/e2e/spec.cy.js
+++ pigEbank/frontend/cypress/e2e/spec.cy.js
@@ -14,19 +14,58 @@ describe('Fail Login', () => {
 })
 
 describe('Login Sucess', () => {
-  it('logging in with an existing user should redirect the user to the dashboard and add a user cookie', () => {
+  it('logging in with all existing users should redirect the user to the dashboard and add a user cookie', async () => {
     cy.visit('http://localhost:3000/');
-
-    cy.contains("Login").click();
-    cy.url().should('include', "/login")
     
-    cy.get('.form-control[type="email"]').type('huffandpuff@threepigs.edu');
-    cy.get('.form-control[type="password"]').type('Refrence1!');
+    let accobj;
+    let gotObj = false;
+    await cy.request('GET', 'http://localhost:8080/accounts').then(response => {
+      accobj = response.body;
+      console.log('this is response body');
+      console.log(response.body);
+      gotObj = true;
+    });
 
-    cy.contains("Login").click();
-    cy.url().should('include', "/dashboard");
-  })
-})
+       
+    accobj.forEach( account => {
+      if(account.email != null){
+        cy.contains("Login").click();
+        cy.url().should('include', "/login");
+
+        cy.get('.form-control[type="email"]').type(account.email);
+        cy.get('.form-control[type="password"]').type(account.password);
+
+        cy.contains("Login").click();
+        cy.url().should('include', "/dashboard");
+
+      
+
+        cy.getCookie('username').then( cookie => {
+          if(cookie) {
+            expect(cookie.value).to.eq(account.username);
+          } else {
+            console.log('failed to get ' + account.username + ' username cookie');
+          }
+        });
+
+        cy.getCookie('email').then( cookie => {
+          if(cookie) {
+            expect(cookie.value).to.eq(account.email);
+          } else {
+            console.log('failed to get ' + account.username + ' email cookie');
+          }
+        })
+
+        cy.contains('Log Out').click();
+      } else {
+        console.log('account with null email skipped');
+      }
+
+    });
+      
+
+  });
+});
 
 /*
 describe('Create Account', () => {
@@ -50,6 +89,24 @@ describe('Create Account', () => {
 
   })
 })
+
+
+describe('Create New Goal', () => {
+  it('After login, can the user make a pig', () => {
+    cy.visit('http://localhost:3000/login');
+
+    cy.get('.form-control[type="email"]').type('huffandpuff@threepigs.edu');
+    cy.get('.form-control[type="password"]').type('Refrence1!');
+
+    cy.contains("Login").click();
+    cy.url().should('include', "/dashboard");
+
+    cy.get('[href="/createNewGoal"]').eq(0).click();
+    cy.url().should('include', "/createNewGoal");
+    
+  
+  })
+})
+
+
 */
-
-
