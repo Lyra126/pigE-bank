2e0016923a8b8bee95ad1fe3011dc729dd1478fd
MichaelJK2143 <michaeljohnknauf@gmail.com>
2024-03-26 21:24:37 -0400

    cypress end to end tests

--- pigEbank/frontend/cypress/e2e/spec.cy.js
+++ pigEbank/frontend/cypress/e2e/spec.cy.js
@@ -1,10 +1,55 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
+describe('Fail Login', () => {
+  it('Prove that logging in with a user that isn\'t in the database fails', () => {
     cy.visit('http://localhost:3000/');
 
     cy.contains("Login").click();
     cy.url().should('include', "/login")
-    cy.get('email').type('thisDoesntExist@gmail.com');
-    cy.get('password').type('notReal');
+    
+    cy.get('.form-control[type="email"]').type('thisDoesntExist@gmail.com');
+    cy.get('.form-control[type="password"]').type('notReal');
+
+    cy.contains("Login").click();
+    cy.contains("Login failed. Invalid email or password.").should('be.visible');
   })
 })
+
+describe('Login Sucess', () => {
+  it('logging in with an existing user should redirect the user to the dashboard and add a user cookie', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.contains("Login").click();
+    cy.url().should('include', "/login")
+    
+    cy.get('.form-control[type="email"]').type('huffandpuff@threepigs.edu');
+    cy.get('.form-control[type="password"]').type('Refrence1!');
+
+    cy.contains("Login").click();
+    cy.url().should('include', "/dashboard");
+  })
+})
+
+/*
+describe('Create Account', () => {
+  it('Checks backend to see that account is successfully created, user cookie created, and redirected to dashboard', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.contains('Create Account').click();
+    cy.url().should('include', '/createAccount');
+
+    cy.get('.form-control[placeholder="Enter your first name"]').type('testName');
+    cy.get('.form-control[placeholder="Enter your last name"]').type('testLast');
+
+    cy.get('.form-control[placeholder="Enter your email"]').type('test@cypress.com');
+    cy.get('.form-control[placeholder="Enter Username"]').type('testUsername');
+    cy.get('[type="password"]').eq(0).type('Testpassword1!');
+    cy.get('[type="password"]').eq(1).type('Testpassword1!');
+
+
+    cy.contains('Create Account').click();
+
+
+  })
+})
+*/
+
+
